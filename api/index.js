eec39b70cbcdb8cb8f927584402c51c89aef2300
rahulkumar <107376311+flashpogu@users.noreply.github.com>
2024-04-11 16:48:49 +0530

    deployment pre requisite

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -19,6 +22,12 @@ const PORT = process.env.PORT || 9000;
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
